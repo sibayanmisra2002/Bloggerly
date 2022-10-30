f2654ffcd3d4c4019526c02a9d94398aeac761c1
vishaaal <officialvk02@gmail.com>
2022-10-30 13:04:42 +0530

    [fix] import

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,4 +1,4 @@
-import "./Login.css";
+import "./login.css";
 import { useNavigate } from 'react-router-dom';
 import styled from "styled-components";
 import { FaFacebookF, FaGoogle, FaGithub } from "react-icons/fa";
